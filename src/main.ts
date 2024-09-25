f530e74b196ad81a57af680c3c374e9aeb093f05
ThieryMichel <thiery@marmelab.com>
2024-09-25 16:06:42 +0200

    run action on pull_request

--- src/main.ts
+++ src/main.ts
@@ -244,7 +244,7 @@ async function main() {
     readFileSync(process.env.GITHUB_EVENT_PATH ?? "", "utf8")
   );
 
-  if (eventData.action === "opened") {
+  if (eventData.action === "opened" || eventData.action === "pull_request") {
     diff = await getDiff(
       prDetails.owner,
       prDetails.repo,
